df7615d6920793a3a2d054b8dc44811e6c780bcc
Jack Lowry <jack.lowry@mail.utoronto.ca>
2022-01-30 14:18:11 -0500

    can now scrape and return relevant HTML from cryptoships page

--- script.js
+++ script.js
@@ -31,4 +31,50 @@ async function getPageStuff() {  //returns [tab url, raw HTML]
     return [tab.url, scraped[0].result]
 }
 
-const pageStuff = getPageStuff()
+async function getter() {
+    const result = await getPageStuff()
+    localStorage.setItem("pageURL", result[0])
+    localStorage.setItem("pageHTML", result[1])
+}
+
+getter()
+
+let pageURL = localStorage.getItem("pageURL")
+let pageHTML = localStorage.getItem("pageHTML")
+
+
+function findEndDiv(html, div) { //returns [pos of opening <div> => int, pos of closing </div> => int], returns -1 if error
+    const start = html.search(div)
+
+    let found = false
+    let divOpens = 1
+    let index = start + 1
+    while (found === false) {
+        let nextDivOpen = html.slice(index).search('<div')
+        if (nextDivOpen === -1) { nextDivOpen = 10 * (10 ** 10) }
+        let nextDivClose = html.slice(index).search('</div')
+        if (nextDivClose === -1) { nextDivClose = 10 * (10 ** 10) }
+
+        if (divOpens === 0) {
+            found = true
+            return [start, index + nextDivClose]
+        }
+
+        if (nextDivOpen < nextDivClose) {
+            divOpens += 1
+            index = index + nextDivOpen + 1
+        } else if (nextDivClose < nextDivOpen) {
+            divOpens -= 1
+            index = index + nextDivClose + 1
+        } else {
+            return -1
+        }
+
+    }
+    return -1
+}
+
+function getWithinDiv(html, div) { //returns relevant sliced html within input div tag
+    let divs = findEndDiv(html, div)
+    return html.slice(divs[0], divs[1])
+}
